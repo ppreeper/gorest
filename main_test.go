0bac323db72615ceaf19414dbacc6e6a02512709
Peter Preeper <ppreeper@gmail.com>
2017-03-12 12:13:21 -0600

    testing added ensureTableExists and clearTable

--- main_test.go
+++ main_test.go
@@ -25,3 +25,22 @@ func TestMain(m *testing.M) {
 
     os.Exit(code)
 }
+
+func ensureTableExists() {
+    if _, err := a.DB.Exec(tableCreationQuery)l err != nil {
+        log.Fatal(err)
+    }
+}
+
+func clearTable() {
+    a.DB.Exec("DELETE FROM products")
+    a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+}
+
+const tableCreationQuery = 'CREATE TABLE IF NOT EXISTS products
+(
+    id SERIAL,
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL DEFAULT 0.00,
+    CONSTRAINT products_pkey PRIMARY KEY (id)
+)'
