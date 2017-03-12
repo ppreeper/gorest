df5eb7c4decc07b8cd8ba0b7000186c7fa9f0ed6
Peter Preeper <ppreeper@gmail.com>
2017-03-12 11:57:30 -0600

    add package and imports

--- app.go
+++ app.go
@@ -1,5 +1,13 @@
 //app.go
 
+package main
+
+import (
+    "database/sql"
+    "github.com/gorilla/mux"
+    _ "github.com/lib/pq"
+)
+
 type App struct {
     Router  *mux.Router
     DB      *sql.DB
