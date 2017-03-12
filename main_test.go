18ba65d7f3302fa717595f96bd8e2ccfa49db569
Peter Preeper <ppreeper@gmail.com>
2017-03-12 12:10:17 -0600

    testing structure

--- main_test.go
+++ main_test.go
@@ -8,4 +8,20 @@ import (
     "."
 )
 
+var a main.App
 
+func TestMain(m *testing.M) {
+    a = main.App()
+    a.Initialize(
+        os.Getenv("TEST_DB_USERNAME"),
+        os.Getenv("TEST_DB_PASSWORD"),
+        os.Getenv("TEST_DB_NAME"))
+
+    ensureTableExists()
+
+    code := m.Run()
+
+    clearTable()
+
+    os.Exit(code)
+}
